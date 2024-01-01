d4ec07af40def81595a8080aa21d0b3a83681aa7
Weelonce <bkamolov575@gmail.com>
2024-01-01 18:12:28 +0500

    initial commit

---
+++ talaba.py
@@ -0,0 +1,13 @@
+class Shaxs:
+    def __init__(self, ism,familiya,passport,tyil,idraqam) -> None:
+        self.ism = ism
+        self.familiya= familiya
+        self.passport = passport
+        
+
+
+
+
+class Talaba:
+    def __init__(self, ism,familiya,passport,tyil,idraqam,manzil) -> None:
+        pass
